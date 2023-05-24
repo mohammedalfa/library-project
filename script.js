d1ccd5e5401fb700d4cf1c093edeb0bbba7958ec
mohammedalfa <mohammedrafid2006@yahoo.com>
2023-05-23 22:08:56 -0400

    add additional information to book object

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, image) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
+  this.image = image;
   this.info = function() {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${read ? 'read' : 'not read yet'}`;
+    return `${this.title} by ${this.author}, ${this.pages} pages, read: ${this.read}`;
   };
 }
 
@@ -18,21 +19,32 @@ const cardContainer = document.querySelector(".container");
 
 function addCard(book) {
   const card = document.createElement("div");
+  const image = document.createElement("img");
   const title = document.createElement("div");
   const author = document.createElement("div");
   const pages = document.createElement("div");
   const read = document.createElement("div");
+  const readCheckbox = document.createElement("input");
+  const removeButton = document.createElement("button");
   card.classList.add("card");
   title.classList.add("title");
   author.classList.add("author");
   pages.classList.add("pages");
   read.classList.add("read");
+  readCheckbox.id = "read-check";
+  removeButton.classList.add("remove");
+  image.src = book.image;
   title.textContent = `Title: ${book.title}`;
   author.textContent = `Author: ${book.author}`;
   pages.textContent = `Pages: ${book.pages}`;
-  read.textContent = `read: ${book.read}`;
-  card.appendChild(title, author, pages, read);
-  cardContainer.append(card);
+  read.textContent = `Read: `;
+  readCheckbox.type = "checkbox";
+  removeButton.type = "button";
+  removeButton.textContent = "Remove";
+  readCheckbox.checked = +book.read ? true : false;
+  read.appendChild(readCheckbox);
+  card.append(image, title, author, pages, read, removeButton);
+  cardContainer.appendChild(card);
 }
 
 const addBookButton = document.querySelector("#add-book");
@@ -51,7 +63,7 @@ const addButton = document.querySelector(".modal #add");
 bookForm.addEventListener("submit", (e) => {
   e.preventDefault();
   let bookInfo = Object.fromEntries(new FormData(e.target));
-  addBookToLibrary(bookInfo.title, bookInfo.author, bookInfo.pages, bookInfo.read);
+  addBookToLibrary(bookInfo.title, bookInfo.author, bookInfo.pages, bookInfo.read, bookInfo.image);
   cancelButton.click();
   console.log(myLibrary);
   addCard(bookInfo);
