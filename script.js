5d6a5a8f0bf8c192bfdd28150d78d95c3afb039d
mohammedalfa <mohammedrafid2006@yahoo.com>
2023-05-23 21:38:45 -0400

    add ability to add cards for books

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -8,5 +10,50 @@ function Book(title, author, pages, read) {
   };
 }
 
-const aBook = new Book('HP', 'JK', 100, false);
-alert(aBook.info());
+function addBookToLibrary(title, author, pages, read) {
+  myLibrary.push(new Book(title, author, pages, read));
+}
+
+const cardContainer = document.querySelector(".container");
+
+function addCard(book) {
+  const card = document.createElement("div");
+  const title = document.createElement("div");
+  const author = document.createElement("div");
+  const pages = document.createElement("div");
+  const read = document.createElement("div");
+  card.classList.add("card");
+  title.classList.add("title");
+  author.classList.add("author");
+  pages.classList.add("pages");
+  read.classList.add("read");
+  title.textContent = `Title: ${book.title}`;
+  author.textContent = `Author: ${book.author}`;
+  pages.textContent = `Pages: ${book.pages}`;
+  read.textContent = `read: ${book.read}`;
+  card.appendChild(title, author, pages, read);
+  cardContainer.append(card);
+}
+
+const addBookButton = document.querySelector("#add-book");
+const addBookModal = document.querySelector(".modal");
+addBookButton.addEventListener("click", () => {
+  addBookModal.style.display = "block";
+});
+
+const cancelButton = document.querySelector(".modal #cancel");
+cancelButton.addEventListener("click", () => {
+  addBookModal.style.display = "none";
+});
+
+const bookForm = document.querySelector("#book-info");
+const addButton = document.querySelector(".modal #add");
+bookForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let bookInfo = Object.fromEntries(new FormData(e.target));
+  addBookToLibrary(bookInfo.title, bookInfo.author, bookInfo.pages, bookInfo.read);
+  cancelButton.click();
+  console.log(myLibrary);
+  addCard(bookInfo);
+});
+
